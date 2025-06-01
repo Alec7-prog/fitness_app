33acea7c3f39bddfc25ba4047ed0effd7be80d61
Alec7-prog <56138258+Alec7-prog@users.noreply.github.com>
2025-06-01 18:09:53 -0500

    #	modified:   pubspec.yaml

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Breakfast')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset('assets/icons/leftArrow.svg'),
+          ),
+        ),
+
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 37,
+              decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/menu-dots.svg',
+                height: 30,
+                width: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
